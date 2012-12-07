625cb53464a18ff139ae2badd109b64f231ebbfe
emilienm <emilien.macchi@enovance.com>
2012-12-07 15:52:50 +0100

    Move settings to init.pp

--- manifests/api.pp
+++ manifests/api.pp
@@ -1,17 +1,4 @@
 class ceilometer::api(
-  $keystone_password,
-  $verbose = 'False',
-  $debug = 'False',
-  $rabbit_host = '127.0.0.1',
-  $rabbit_port = 5672,
-  $rabbit_userid = 'guest',
-  $rabbit_password = '',
-  $rabbit_virtualhost = '/',
-  $database_connection = 'mysql://ceilometer:ceilometer@127.0.0.1/ceilometer',
-  $keystone_host = '127.0.0.1',
-  $keystone_port = '35357',
-  $keystone_protocol = 'http',
-  $keystone_user = 'ceilometer',
   $enabled = true,
 ) {
 
@@ -19,29 +6,11 @@ class ceilometer::api(
     ensure => installed
   }
 
-  file { ['/etc/ceilometer/ceilometer.conf']:
-  }
-
   if $enabled {
     $service_ensure = 'running'
   } else {
     $service_ensure = 'stopped'
   }
-
-  ceilometer_setting {
-    'DEFAULT/rabbit_host': value => $rabbit_host;
-    'DEFAULT/rabbit_port': value => $rabbit_port;
-    'DEFAULT/rabbit_userid': value => $rabbit_userid;
-    'DEFAULT/rabbit_password': value => $rabbit_password;
-    'DEFAULT/rabbit_virtualhost': value => $rabbit_virtualhost;
-    'DEFAULT/debug': value => $debug;
-    'DEFAULT/verbose': value => $verbose;
-    'DEFAULT/database_connection': value => $database_connection;
-    'keystone_authtoken/auth_host': value => $keystone_host;
-    'keystone_authtoken/auth_port': value => $keystone_port;
-    'keystone_authtoken/protocol': value => $keystone_protocol;
-  }
-
 
   service { 'ceilometer-api':
     name	=> $::ceilometer::params::api_service_name
--- manifests/collector.pp
+++ manifests/collector.pp
@@ -1,39 +1,9 @@
 class ceilometer::collector(
-  $keystone_password,
-  $verbose = 'False',
-  $debug = 'False',
-  $rabbit_host = '127.0.0.1',
-  $rabbit_port = 5672,
-  $rabbit_userid = 'guest',
-  $rabbit_password = '',
-  $rabbit_virtualhost = '/',
-  $database_connection = 'mysql://ceilometer:ceilometer@127.0.0.1/ceilometer',
-  $keystone_host = '127.0.0.1',
-  $keystone_port = '35357',
-  $keystone_protocol = 'http',
-  $keystone_user = 'ceilometer',
   $enabled = true,
 ) {
 
   package { 'ceilometer-collector':
     ensure => installed
-  }
-
-  ceilometer_setting {
-    'DEFAULT/rabbit_host': value => $rabbit_host;
-    'DEFAULT/rabbit_port': value => $rabbit_port;
-    'DEFAULT/rabbit_userid': value => $rabbit_userid;
-    'DEFAULT/rabbit_password': value => $rabbit_password;
-    'DEFAULT/rabbit_virtualhost': value => $rabbit_virtualhost;
-    'DEFAULT/debug': value => $debug;
-    'DEFAULT/verbose': value => $verbose;
-    'DEFAULT/database_connection': value => $database_connection;
-    'keystone_authtoken/auth_host': value => $keystone_host;
-    'keystone_authtoken/auth_port': value => $keystone_port;
-    'keystone_authtoken/protocol': value => $keystone_protocol;
-  }
-
-  file { ['/etc/ceilometer/ceilometer.conf']:
   }
 
   if $enabled {
