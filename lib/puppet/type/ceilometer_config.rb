699aab14dfa21813c11ad0b3bd6aecfa30c8b361
emilienm <emilien.macchi@enovance.com>
2012-12-07 14:33:17 +0100

    Add ceilometer_setting for type/provider

---
+++ lib/puppet/type/ceilometer_config.rb
@@ -0,0 +1,18 @@
+Puppet::Type.newtype(:ceilometer_config) do
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'Section/setting name to manage from ceilometer.conf'
+    newvalues(/\S+\/\S+/)
+  end
+
+  newproperty(:value) do
+    desc 'The value of the setting to be defined.'
+    munge do |v|
+      v.to_s.strip
+    end
+  end
+
+
+end
