3a2b7e1acabdbb35292329f941b37f8bb9580c5a
François Charlier <francois.charlier@enovance.com>
2013-06-06 15:41:45 +0200

    Add auth_admin_prefix support for ceilometer::api
    To support keystone urls like http://host:80/keystone/admin
    
    Implements blueprint serve-keystone-from-wsgi
    
    Change-Id: I68c17a5b76ed61b18337ee580fba63200d874792

--- spec/classes/ceilometer_api_spec.rb
+++ spec/classes/ceilometer_api_spec.rb
@@ -52,6 +52,30 @@ describe 'ceilometer::api' do
       should contain_ceilometer_config('keystone_authtoken/admin_tenant_name').with_value( params[:keystone_tenant] )
       should contain_ceilometer_config('keystone_authtoken/admin_user').with_value( params[:keystone_user] )
       should contain_ceilometer_config('keystone_authtoken/admin_password').with_value( params[:keystone_password] )
+      should contain_ceilometer_config('keystone_authtoken/auth_admin_prefix').with_ensure('absent')
+    end
+
+    context 'when specifying keystone_auth_admin_prefix' do
+      describe 'with a correct value' do
+        before { params['keystone_auth_admin_prefix'] = '/keystone/admin' }
+        it { should contain_ceilometer_config('keystone_authtoken/auth_admin_prefix').with_value('/keystone/admin') }
+      end
+
+      [
+        '/keystone/',
+        'keystone/',
+        'keystone',
+        '/keystone/admin/',
+        'keystone/admin/',
+        'keystone/admin'
+      ].each do |auth_admin_prefix|
+        describe "with an incorrect value #{auth_admin_prefix}" do
+          before { params['keystone_auth_admin_prefix'] = auth_admin_prefix }
+
+          it { expect { should contain_ceilomete_config('keystone_authtoken/auth_admin_prefix') }.to \
+            raise_error(Puppet::Error, /validate_re\(\): "#{auth_admin_prefix}" does not match/) }
+        end
+      end
     end
   end
 
