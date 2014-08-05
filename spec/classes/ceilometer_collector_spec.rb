8c9f4fcd53e4ef0fb7694de6dce6e001c7e8cf7d
Mathieu Gagné <mgagne@iweb.com>
2014-08-05 11:58:27 -0400

    Add ability to disable ceilometer-collector without db
    You are no longer required to include ceilometer::db when
    disabling ceilometer-collector using the enabled parameter.
    
    The relationships with any database resources are now only enforced
    when the service is enabled.
    
    Closes-bug: #1352958
    Change-Id: I00a5e651184788acdd30de1908e2bf38566f78e1

--- spec/classes/ceilometer_collector_spec.rb
+++ spec/classes/ceilometer_collector_spec.rb
@@ -8,22 +8,40 @@ describe 'ceilometer::collector' do
 
   shared_examples_for 'ceilometer-collector' do
 
-    it { should contain_class('ceilometer::params') }
+    context 'when enabled' do
+      before do
+        pre_condition << "class { 'ceilometer::db': }"
+      end
 
-    it 'installs ceilometer-collector package' do
-      should contain_package(platform_params[:collector_package_name])
+      it { should contain_class('ceilometer::params') }
+
+      it 'installs ceilometer-collector package' do
+        should contain_package(platform_params[:collector_package_name])
+      end
+
+      it 'configures ceilometer-collector service' do
+        should contain_service('ceilometer-collector').with(
+          :ensure     => 'running',
+          :name       => platform_params[:collector_service_name],
+          :enable     => true,
+          :hasstatus  => true,
+          :hasrestart => true
+        )
+      end
+
+      it 'configures relationships on database' do
+        should contain_class('ceilometer::db').with_before('Service[ceilometer-collector]')
+        should contain_exec('ceilometer-dbsync').with_notify('Service[ceilometer-collector]')
+      end
     end
 
-    it 'configures ceilometer-collector service' do
-      should contain_service('ceilometer-collector').with(
-        :ensure     => 'running',
-        :name       => platform_params[:collector_service_name],
-        :enable     => true,
-        :hasstatus  => true,
-        :hasrestart => true,
-        :require    => 'Class[Ceilometer::Db]',
-        :subscribe  => 'Exec[ceilometer-dbsync]'
-      )
+    context 'when disabled' do
+      let :params do
+        { :enabled => false }
+      end
+
+      # Catalog compilation does not crash for lack of ceilometer::db
+      it { should compile }
     end
   end
 
