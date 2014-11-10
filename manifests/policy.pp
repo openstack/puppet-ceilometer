1d78470e5023e2ca6adef47c904d14d046dd87d1
Yanis Guenane <yanis.guenane@enovance.com>
2014-11-10 14:45:07 -0500

    policy.pp : Fix duplicate ressource issue openstacklib::policy
    Currently, in the policy.pp file the class openstacklib::policy
    is called. The same apply for all components resulting in a
    Duplicate Ressource issue. Using directly the underlying ressource
    openstacklib::policy::base we prevent this issue.
    
    Change-Id: Ia384a42365056317ca14031081f8c6e134f266aa

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -18,11 +18,12 @@ class ceilometer::policy (
   $policy_path = '/etc/ceilometer/policy.json',
 ) {
 
+  validate_hash($policies)
+
   Openstacklib::Policy::Base {
     file_path => $policy_path,
   }
-  class { 'openstacklib::policy' :
-    policies => $policies,
-  }
+
+  create_resources('openstacklib::policy::base', $policies)
 
 }
