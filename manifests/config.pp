2e0affcd93c566452a5d381cff4a5a76634d93e6
ZhongShengping <chdzsp@163.com>
2016-02-27 07:25:53 +0800

    Add api_paste type/provider for Ceilometer
    Add ceilometer_api_paste_ini type/provider for Ceilometer.
    Also add the capability to configure api-paste.ini with config.pp.
    Closes-bug: #1483371
    
    Change-Id: I7a5ce9629b9069495e233a4576742aeb171a09c1

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,13 +19,20 @@
 #     DEFAULT/bar:
 #       value: barValue
 #
+# [*ceilometer_api_paste_ini*]
+#   (optional) Allow configuration of /etc/ceilometer/api-paste.ini options.
+#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class ceilometer::config (
-  $ceilometer_config         = {},
+  $ceilometer_config        = {},
+  $ceilometer_api_paste_ini = {},
 ) {
+
   validate_hash($ceilometer_config)
+  validate_hash($ceilometer_api_paste_ini)
 
   create_resources('ceilometer_config', $ceilometer_config)
+  create_resources('ceilometer_api_paste_ini', $ceilometer_api_paste_ini)
 }
