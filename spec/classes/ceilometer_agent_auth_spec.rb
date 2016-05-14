c7c55b4ace8080edb80867a19f43ac3c730fc341
Matt Fischer <matt@mattfischer.com>
2016-05-13 20:22:16 -0600

    Correct service_credentials section
    The service_credential section variables were renamed at some point
    before Mitaka. They are broken in Mitaka and Newton, and possibly
    earlier. Additionally some critical variables for dealing with domains
    and keystone v3 are missing. This corrects those issues.
    
    A later commit should deprecate some of the naming for things
    that have totally different names, like the endpoint_type and
    tenant_name, but we need to cleanly (and minimally) backport this to
    Mitaka.
    
    Change-Id: I8de5e42102fed2de6bea7bbdd788e86a88f0354c
    Closes-Bug: #1581691
    (cherry picked from commit c853fc65013ee8ce78c42e822df98b423d25bd48)

--- spec/classes/ceilometer_agent_auth_spec.rb
+++ spec/classes/ceilometer_agent_auth_spec.rb
@@ -18,24 +18,33 @@ describe 'ceilometer::agent::auth' do
   shared_examples_for 'ceilometer-agent-auth' do
 
     it 'configures authentication' do
-      is_expected.to contain_ceilometer_config('service_credentials/os_auth_url').with_value('http://localhost:5000/v2.0')
-      is_expected.to contain_ceilometer_config('service_credentials/os_region_name').with_value('<SERVICE DEFAULT>')
-      is_expected.to contain_ceilometer_config('service_credentials/os_username').with_value('ceilometer')
-      is_expected.to contain_ceilometer_config('service_credentials/os_password').with_value('password')
-      is_expected.to contain_ceilometer_config('service_credentials/os_password').with_value(params[:auth_password]).with_secret(true)
-      is_expected.to contain_ceilometer_config('service_credentials/os_tenant_name').with_value('services')
-      is_expected.to contain_ceilometer_config('service_credentials/os_cacert').with(:ensure => 'absent')
+      is_expected.to contain_ceilometer_config('service_credentials/auth_url').with_value('http://localhost:5000/v2.0')
+      is_expected.to contain_ceilometer_config('service_credentials/region_name').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ceilometer_config('service_credentials/username').with_value('ceilometer')
+      is_expected.to contain_ceilometer_config('service_credentials/password').with_value('password')
+      is_expected.to contain_ceilometer_config('service_credentials/password').with_value(params[:auth_password]).with_secret(true)
+      is_expected.to contain_ceilometer_config('service_credentials/project_name').with_value('services')
+      is_expected.to contain_ceilometer_config('service_credentials/ca_file').with(:ensure => 'absent')
+      is_expected.to contain_ceilometer_config('service_credentials/user_domain_name').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ceilometer_config('service_credentials/project_domain_name').with_value('<SERVICE DEFAULT>')
+      is_expected.to contain_ceilometer_config('service_credentials/auth_type').with_value('<SERVICE DEFAULT>')
     end
 
     context 'when overriding parameters' do
       before do
         params.merge!(
-          :auth_cacert        => '/tmp/dummy.pem',
-          :auth_endpoint_type => 'internalURL',
+          :auth_cacert               => '/tmp/dummy.pem',
+          :auth_endpoint_type        => 'internalURL',
+          :auth_type                 => 'password',
+          :auth_user_domain_name     => 'MyDomain',
+          :auth_project_domain_name  => 'MyProjDomain',
         )
       end
-      it { is_expected.to contain_ceilometer_config('service_credentials/os_cacert').with_value(params[:auth_cacert]) }
-      it { is_expected.to contain_ceilometer_config('service_credentials/os_endpoint_type').with_value(params[:auth_endpoint_type]) }
+      it { is_expected.to contain_ceilometer_config('service_credentials/ca_file').with_value(params[:auth_cacert]) }
+      it { is_expected.to contain_ceilometer_config('service_credentials/interface').with_value(params[:auth_endpoint_type]) }
+      it { is_expected.to contain_ceilometer_config('service_credentials/user_domain_name').with_value(params[:auth_user_domain_name]) }
+      it { is_expected.to contain_ceilometer_config('service_credentials/project_domain_name').with_value(params[:auth_project_domain_name]) }
+      it { is_expected.to contain_ceilometer_config('service_credentials/auth_type').with_value(params[:auth_type]) }
     end
 
   end
