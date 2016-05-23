40b84c310e6b78226a08e7c04865c037542dfc54
Iury Gregory Melo Ferreira <iurygregory@gmail.com>
2016-05-22 21:09:03 -0300

    Deprecate use_syslog
    [DEFAULT/use_syslog] was deprecated in Mitaka [1]
    init.pp logging.pp - deprecate use_syslog, add warning, remove reference
    tests - remove use_syslog and [DEFAULT/use_syslog] verification.
    
    [1]
    http://docs.openstack.org/mitaka/config-reference/tables/conf-changes/ceilometer.html
    
    Change-Id: I352e55586bfb9a030665175dc73a27db35a21fcf

--- manifests/init.pp
+++ manifests/init.pp
@@ -39,10 +39,6 @@
 #    If set to boolean false, it will not log to any directory.
 #    Defaults to undef.
 #
-#  [*use_syslog*]
-#    (Optional) Use syslog for logging
-#    Defaults to undef.
-#
 #  [*use_stderr*]
 #    (Optional) Use stderr for logging
 #    Defaults to undef.
@@ -215,6 +211,11 @@
 #  [*verbose*]
 #    (Optional) Deprecated. should the daemons log verbose messages.
 #    Defaults to undef.
+#
+#  [*use_syslog*]
+#    (Optional) Deprecated. Use syslog for logging
+#    Defaults to undef.
+#
 class ceilometer(
   $http_timeout                       = '600',
   $event_time_to_live                 = '-1',
@@ -224,7 +225,6 @@ class ceilometer(
   $package_ensure                     = 'present',
   $debug                              = undef,
   $log_dir                            = undef,
-  $use_syslog                         = undef,
   $use_stderr                         = undef,
   $log_facility                       = undef,
   $rpc_backend                        = $::os_service_default,
@@ -266,6 +266,7 @@ class ceilometer(
   $alarm_history_time_to_live         = undef,
   $metering_secret                    = undef,
   $verbose                            = undef,
+  $use_syslog                         = undef,
 ) {
 
   include ::ceilometer::logging
@@ -274,6 +275,12 @@ class ceilometer(
   if $verbose {
     warning('verbose is deprecated, has no effect and will be removed after Newton cycle.')
   }
+
+  if $use_syslog {
+    warning('use_syslog is deprecated, has no effect and will be removed after Newton cycle.')
+  }
+
+
   # Cleanup in Ocata.
   if $telemetry_secret {
     validate_string($telemetry_secret)
--- spec/classes/ceilometer_logging_spec.rb
+++ spec/classes/ceilometer_logging_spec.rb
@@ -23,7 +23,6 @@ describe 'ceilometer::logging' do
      :instance_format => '[instance: %(uuid)s] ',
      :instance_uuid_format => '[instance: %(uuid)s] ',
      :log_date_format => '%Y-%m-%d %H:%M:%S',
-     :use_syslog => true,
      :use_stderr => false,
      :log_facility => 'LOG_FOO',
      :log_dir => '/var/log',
@@ -55,7 +54,6 @@ describe 'ceilometer::logging' do
 
   shared_examples 'basic default logging settings' do
     it 'configures ceilometer logging settins with default values' do
-      is_expected.to contain_ceilometer_config('DEFAULT/use_syslog').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_ceilometer_config('DEFAULT/use_stderr').with(:value => '<SERVICE DEFAULT>')
       is_expected.to contain_ceilometer_config('DEFAULT/log_dir').with(:value => '/var/log/ceilometer')
       is_expected.to contain_ceilometer_config('DEFAULT/debug').with(:value => '<SERVICE DEFAULT>')
@@ -64,7 +62,6 @@ describe 'ceilometer::logging' do
 
   shared_examples 'basic non-default logging settings' do
     it 'configures ceilometer logging settins with non-default values' do
-      is_expected.to contain_ceilometer_config('DEFAULT/use_syslog').with(:value => 'true')
       is_expected.to contain_ceilometer_config('DEFAULT/use_stderr').with(:value => 'false')
       is_expected.to contain_ceilometer_config('DEFAULT/syslog_log_facility').with(:value => 'LOG_FOO')
       is_expected.to contain_ceilometer_config('DEFAULT/log_dir').with(:value => '/var/log')
