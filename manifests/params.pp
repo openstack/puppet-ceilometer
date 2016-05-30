5ca2819480a168fba15e84821320769284d0c8fc
ZhongShengping <chdzsp@163.com>
2016-05-31 00:45:30 +0800

    Remove useless parameters
    Switch db related parameters to oslo, the pymongo_package_name,
    pymysql_package_name and sqlite_package_name parmaters are
    useless, so delete them.
    
    Change-Id: I67a11f38b2998ee0f29a4aaa179c7d41c10a70cd

--- manifests/params.pp
+++ manifests/params.pp
@@ -26,12 +26,9 @@ class ceilometer::params {
       $agent_polling_service_name      = 'openstack-ceilometer-polling'
       $api_service_name                = 'openstack-ceilometer-api'
       $collector_service_name          = 'openstack-ceilometer-collector'
-      $pymongo_package_name            = 'python-pymongo'
       $agent_notification_service_name = 'openstack-ceilometer-notification'
       $ceilometer_wsgi_script_path     = '/var/www/cgi-bin/ceilometer'
       $ceilometer_wsgi_script_source   = '/usr/lib/python2.7/site-packages/ceilometer/api/app.wsgi'
-      $sqlite_package_name             = undef
-      $pymysql_package_name            = undef
     }
     'Debian': {
       # package names
@@ -50,10 +47,6 @@ class ceilometer::params {
       $collector_service_name          = 'ceilometer-collector'
       $api_service_name                = 'ceilometer-api'
       $agent_notification_service_name = 'ceilometer-agent-notification'
-      # db packages
-      $pymongo_package_name            = 'python-pymongo'
-      $sqlite_package_name             = 'python-pysqlite2'
-      $pymysql_package_name            = 'python-pymysql'
 
       # Operating system specific
       case $::operatingsystem {
