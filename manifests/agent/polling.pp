4e576d3f5393cb58b2d19b940d9a3c3d81144417
Matthew J. Black <mjblack@gmail.com>
2016-09-23 19:27:04 -0400

    Remove blank values in namespaces array.
    The inline template is using compact which is not removing
    the blank values. A call to delete function from stdlib is
    needed to remove the blank values.
    
    Change-Id: I7cf3ca383572d569a7e89431dcc39385605cf820

--- manifests/agent/polling.pp
+++ manifests/agent/polling.pp
@@ -78,7 +78,7 @@ class ceilometer::agent::polling (
     }
   }
 
-  $namespaces = [$central_namespace_name, $compute_namespace_name, $ipmi_namespace_name]
+  $namespaces = delete([$central_namespace_name, $compute_namespace_name, $ipmi_namespace_name], '')
   $namespaces_real = inline_template('<%= @namespaces.select { |x| x and x !~ /^undef/ }.compact.join "," %>')
 
   package { 'ceilometer-polling':
