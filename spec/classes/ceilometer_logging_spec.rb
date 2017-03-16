e6643302983cd8ca958e203b30a05835679589a3
Mykyta Karpin <mkarpin@mirantis.com>
2017-03-16 18:40:15 +0200

    oslo log: check puppet resource instead of actual config in spec
    Change-Id: I69498bf80b052b70ea73c0fafc4736678fc3417a

--- spec/classes/ceilometer_logging_spec.rb
+++ spec/classes/ceilometer_logging_spec.rb
@@ -34,77 +34,68 @@ describe 'ceilometer::logging' do
   shared_examples_for 'ceilometer-logging' do
 
     context 'with basic logging options and default settings' do
-      it_configures  'basic default logging settings'
+      it_behaves_like 'basic default logging settings'
     end
 
     context 'with basic logging options and non-default settings' do
       before { params.merge!( log_params ) }
-      it_configures 'basic non-default logging settings'
+      it_behaves_like 'basic non-default logging settings'
     end
 
     context 'with extended logging options' do
       before { params.merge!( log_params ) }
-      it_configures 'logging params set'
+      it_behaves_like 'logging params set'
     end
 
     context 'without extended logging options' do
-      it_configures 'logging params unset'
+      it_behaves_like 'logging params unset'
     end
 
   end
 
   shared_examples 'basic default logging settings' do
     it 'configures ceilometer logging settings with default values' do
-      is_expected.to contain_ceilometer_config('DEFAULT/use_syslog').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_ceilometer_config('DEFAULT/use_stderr').with(:value => '<SERVICE DEFAULT>')
-      is_expected.to contain_ceilometer_config('DEFAULT/log_dir').with(:value => '/var/log/ceilometer')
-      is_expected.to contain_ceilometer_config('DEFAULT/debug').with(:value => '<SERVICE DEFAULT>')
+      is_expected.to contain_oslo__log('ceilometer_config').with(
+        :use_syslog          => '<SERVICE DEFAULT>',
+        :use_stderr          => '<SERVICE DEFAULT>',
+        :syslog_log_facility => '<SERVICE DEFAULT>',
+        :log_dir             => '/var/log/ceilometer',
+        :debug               => '<SERVICE DEFAULT>',
+      )
     end
   end
 
   shared_examples 'basic non-default logging settings' do
     it 'configures ceilometer logging settings with non-default values' do
-      is_expected.to contain_ceilometer_config('DEFAULT/use_syslog').with(:value => 'true')
-      is_expected.to contain_ceilometer_config('DEFAULT/use_stderr').with(:value => 'false')
-      is_expected.to contain_ceilometer_config('DEFAULT/syslog_log_facility').with(:value => 'LOG_FOO')
-      is_expected.to contain_ceilometer_config('DEFAULT/log_dir').with(:value => '/var/log')
-      is_expected.to contain_ceilometer_config('DEFAULT/debug').with(:value => 'true')
+      is_expected.to contain_oslo__log('ceilometer_config').with(
+        :use_syslog          => true,
+        :use_stderr          => false,
+        :syslog_log_facility => 'LOG_FOO',
+        :log_dir             => '/var/log',
+        :debug               => true,
+      )
     end
   end
 
   shared_examples_for 'logging params set' do
     it 'enables logging params' do
-      is_expected.to contain_ceilometer_config('DEFAULT/logging_context_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/logging_default_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/logging_debug_format_suffix').with_value(
-        '%(funcName)s %(pathname)s:%(lineno)d')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/logging_exception_prefix').with_value(
-       '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/log_config_append').with_value(
-        '/etc/ceilometer/logging.conf')
-      is_expected.to contain_ceilometer_config('DEFAULT/publish_errors').with_value(
-        true)
-
-      is_expected.to contain_ceilometer_config('DEFAULT/default_log_levels').with_value(
-        'amqp=WARN,amqplib=WARN,boto=WARN,iso8601=WARN,requests.packages.urllib3.connectionpool=WARN,sqlalchemy=WARN,suds=INFO')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/fatal_deprecations').with_value(
-        true)
-
-      is_expected.to contain_ceilometer_config('DEFAULT/instance_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/instance_uuid_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_ceilometer_config('DEFAULT/log_date_format').with_value(
-        '%Y-%m-%d %H:%M:%S')
+      is_expected.to contain_oslo__log('ceilometer_config').with(
+        :logging_context_format_string =>
+          '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s',
+        :logging_default_format_string => '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s',
+        :logging_debug_format_suffix   => '%(funcName)s %(pathname)s:%(lineno)d',
+        :logging_exception_prefix      => '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s',
+        :log_config_append             => '/etc/ceilometer/logging.conf',
+        :publish_errors                => true,
+        :default_log_levels            => {
+          'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
+          'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
+          'requests.packages.urllib3.connectionpool' => 'WARN' },
+       :fatal_deprecations             => true,
+       :instance_format                => '[instance: %(uuid)s] ',
+       :instance_uuid_format           => '[instance: %(uuid)s] ',
+       :log_date_format                => '%Y-%m-%d %H:%M:%S',
+      )
     end
   end
 
@@ -116,7 +107,7 @@ describe 'ceilometer::logging' do
      :default_log_levels, :fatal_deprecations,
      :instance_format, :instance_uuid_format,
      :log_date_format, ].each { |param|
-        it { is_expected.to contain_ceilometer_config("DEFAULT/#{param}").with_value('<SERVICE DEFAULT>') }
+        it { is_expected.to contain_oslo__log('ceilometer_config').with("#{param}" => '<SERVICE DEFAULT>') }
       }
   end
 
