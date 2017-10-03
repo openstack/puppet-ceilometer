b5c554f759ae5d3906442f59aa755a497a38a490
Pradeep Kilambi <pkilambi@redhat.com>
2017-10-03 09:31:34 -0400

    Add support to set host param
    Lets expose this param so we can override the host param in
    ceilometer config. Without this, some metrics from nova wont
    make it as in some cases the fqdn might not match hostname.
    
    Partial-bug: #1721054
    
    Change-Id: I421a61dc31bd10fec0a10bf7e867001cf4dd3f02
    (cherry picked from commit 4a249ae38c62d2670a37990c7cb44abd77ae3aa4)
    (cherry picked from commit e89477fffd2b2a063b4f144dc13013bdbd118d26)
    (cherry picked from commit df2b2291431619e01a0d843653bc71197f21f903)

--- manifests/init.pp
+++ manifests/init.pp
@@ -221,6 +221,12 @@
 #   in the ceilometer config.
 #   Defaults to false.
 #
+# [*host*]
+#   (Optional) Name of this node. This is typically a hostname, FQDN, or
+#   IP address.
+#   Defaults to $::os_service_default.
+#
+#
 # === DEPRECATED PARAMETERS:
 #  [*metering_secret*]
 #   (optional)  Secret key for signing messages.
@@ -282,6 +288,7 @@ class ceilometer(
   $amqp_username                      = $::os_service_default,
   $amqp_password                      = $::os_service_default,
   $purge_config                       = false,
+  $host                               = $::os_service_default,
   # DEPRECATED PARAMETERS
   $alarm_history_time_to_live         = undef,
   $metering_secret                    = undef,
@@ -381,6 +388,7 @@ class ceilometer(
   # Once we got here, we can act as an honey badger on the rpc used.
   ceilometer_config {
     'DEFAULT/http_timeout'                : value => $http_timeout;
+    'DEFAULT/host'                        : value => $host;
     'publisher/telemetry_secret'          : value => $telemetry_secret_real, secret => true;
     'database/event_time_to_live'         : value => $event_time_to_live;
     'database/metering_time_to_live'      : value => $metering_time_to_live;
