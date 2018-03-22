03db4e5145a5b554742ec6515dabda39cf1dac3c
ZhongShengping <chdzsp@163.com>
2018-03-22 11:09:50 +0800

    Add pool_timeout option
    Add pool_timeout option to configure this value for pool_timeout with
    SQLAlchemy.
    
    Change-Id: Ib84d68f367c043b40f15ec50d45093467f11a4f4
    Closes-Bug: #1757581

--- manifests/db.pp
+++ manifests/db.pp
@@ -44,6 +44,10 @@
 #   (Optional) enable database schema installation.
 #   Defaults to true.
 #
+# [*database_pool_timeout*]
+#   (Optional) If set, use this value for pool_timeout with SQLAlchemy.
+#   Defaults to $::os_service_default
+#
 class ceilometer::db (
   $database_db_max_retries = $::os_service_default,
   $database_connection     = 'mysql+pymysql://ceilometer:ceilometer@localhost/ceilometer',
@@ -53,6 +57,7 @@ class ceilometer::db (
   $database_max_retries    = $::os_service_default,
   $database_retry_interval = $::os_service_default,
   $database_max_overflow   = $::os_service_default,
+  $database_pool_timeout   = $::os_service_default,
   $sync_db                 = true,
 ) {
 
@@ -67,6 +72,7 @@ class ceilometer::db (
     retry_interval => $database_retry_interval,
     max_pool_size  => $database_max_pool_size,
     max_overflow   => $database_max_overflow,
+    pool_timeout   => $database_pool_timeout,
   }
 
   if $sync_db {
