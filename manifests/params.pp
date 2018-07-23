753c8c2cd80da39daa438391683ad59c568e8c03
Tobias Urdin <tobias@busybox.se>
2018-07-23 22:21:21 +0200

    Add support to customize polling meters
    This adds a new parameter called polling_meters
    to the ceilometer::agent::polling class.
    
    If you pass a array of strings this parameter
    will then populate the meters in the polling.yaml
    file by inserting them in the templated polling file.
    
    The default values that was hardcoded in the template
    is still the same but has been moved to the params class
    as an array of strings.
    
    Change-Id: Iae2567b5a0f24b2aa80f9640ff10df96cd656922
    (cherry picked from commit f236f531df8f342c475973620e3f3913399dc2c0)

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,13 +6,40 @@
 class ceilometer::params {
   include ::openstacklib::defaults
 
-  $dbsync_command      = 'ceilometer-upgrade'
-  $expirer_command     = 'ceilometer-expirer'
-  $user                = 'ceilometer'
-  $event_pipeline      = '/etc/ceilometer/event_pipeline.yaml'
-  $pipeline            = '/etc/ceilometer/pipeline.yaml'
-  $polling             = '/etc/ceilometer/polling.yaml'
-  $group               = 'ceilometer'
+  $dbsync_command  = 'ceilometer-upgrade'
+  $expirer_command = 'ceilometer-expirer'
+  $user            = 'ceilometer'
+  $event_pipeline  = '/etc/ceilometer/event_pipeline.yaml'
+  $pipeline        = '/etc/ceilometer/pipeline.yaml'
+  $polling         = '/etc/ceilometer/polling.yaml'
+  $group           = 'ceilometer'
+  $polling_meters  = [
+    'cpu',
+    'cpu_l3_cache',
+    'memory.usage',
+    'network.incoming.bytes',
+    'network.incoming.packets',
+    'network.outgoing.bytes',
+    'network.outgoing.packets',
+    'disk.read.bytes',
+    'disk.read.requests',
+    'disk.write.bytes',
+    'disk.write.requests',
+    'volume.size',
+    'volume.snapshot.size',
+    'volume.backup.size',
+    'hardware.cpu.util',
+    'hardware.memory.used',
+    'hardware.memory.total',
+    'hardware.memory.buffer',
+    'hardware.memory.cached',
+    'hardware.memory.swap.avail',
+    'hardware.memory.swap.total',
+    'hardware.system_stats.io.outgoing.blocks',
+    'hardware.system_stats.io.incoming.blocks',
+    'hardware.network.ip.incoming.datagrams',
+    'hardware.network.ip.outgoing.datagrams',
+  ]
 
   case $::osfamily {
     'RedHat': {
