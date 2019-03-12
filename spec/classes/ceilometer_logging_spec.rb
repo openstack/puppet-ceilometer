201f379a43314dd354fee21a4c894e195708c4d5
ZhongShengping <chdzsp@163.com>
2019-03-12 10:37:37 +0800

    Add log_file parameter
    Change-Id: I6a21a7b5cfbdb616085bd6e0c73ba201901bc242
    Closes-Bug: #1819417

--- spec/classes/ceilometer_logging_spec.rb
+++ spec/classes/ceilometer_logging_spec.rb
@@ -29,6 +29,7 @@ describe 'ceilometer::logging' do
      :use_stderr                     => false,
      :log_facility                   => 'LOG_FOO',
      :log_dir                        => '/var/log',
+     :log_file                       => '/var/log/ceilometer/ceilometer.log',
      :debug                          => true,
     }
   end
@@ -64,6 +65,7 @@ describe 'ceilometer::logging' do
         :use_stderr          => '<SERVICE DEFAULT>',
         :syslog_log_facility => '<SERVICE DEFAULT>',
         :log_dir             => '/var/log/ceilometer',
+        :log_file            => '<SERVICE DEFAULT>',
         :debug               => '<SERVICE DEFAULT>',
       )
     end
@@ -78,6 +80,7 @@ describe 'ceilometer::logging' do
         :use_stderr          => false,
         :syslog_log_facility => 'LOG_FOO',
         :log_dir             => '/var/log',
+        :log_file            => '/var/log/ceilometer/ceilometer.log',
         :debug               => true,
       )
     end
