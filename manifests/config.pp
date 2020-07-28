390cfd1f332bc110ff0dec2df18b7490453b91da
Takashi Kajinami <tkajinam@redhat.com>
2020-07-29 08:05:37 +0900

    Remove api_paste_ini resource
    ... because it is useless since ceilometer-api was removed.
    
    Change-Id: Ida2a8a9bb2b23105db83b6553bfa8b5c783ffc58

--- manifests/config.pp
+++ manifests/config.pp
@@ -19,18 +19,11 @@
 #     DEFAULT/bar:
 #       value: barValue
 #
-# DEPRECATED PARAMETERS
-#
-# [*ceilometer_api_paste_ini*]
-#   (optional) Allow configuration of /etc/ceilometer/api_paste.ini options.
-#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class ceilometer::config (
   $ceilometer_config        = {},
-  # DEPRECATED PARAMETERS
-  $ceilometer_api_paste_ini = undef,
 ) {
 
   include ceilometer::deps
@@ -38,9 +31,5 @@ class ceilometer::config (
   validate_legacy(Hash, 'validate_hash', $ceilometer_config)
 
   create_resources('ceilometer_config', $ceilometer_config)
-
-  if $ceilometer_api_paste_ini != undef {
-    warning('ceilometer_api_paste_ini is deprecated and has no effect.')
-  }
 
 }
