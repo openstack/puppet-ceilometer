2cf3c76ae617db58e4d74fbd83338e8e3e72a6b6
ZhongShengping <chdzsp@163.com>
2020-09-16 11:05:05 +0800

    Set openstackdocs_auto_name to use 'project' as name
    Change-Id: I9ad63fa3c72e4f6aba30a91baf1763030f9af8b0

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -253,3 +253,4 @@ locale_dirs = ['locale/']
 openstackdocs_repo_name = 'openstack/puppet-ceilometer'
 openstackdocs_bug_project = 'puppet-ceilometer'
 openstackdocs_bug_tag = ''
+openstackdocs_auto_name = False
