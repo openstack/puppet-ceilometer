b87511973bde61cb55569dd2b9671c0ead1219c5
ZhongShengping <chdzsp@163.com>
2021-09-14 16:02:08 +0800

    Add watch_log_file option
    Add support for Using logging handler designed to watch file system.
    
    Change-Id: I0ac91a77a1809ee851821bd6f52a43cc6fda890b
    Closes-Bug: #1943212

--- spec/classes/ceilometer_logging_spec.rb
+++ spec/classes/ceilometer_logging_spec.rb
@@ -30,6 +30,7 @@ describe 'ceilometer::logging' do
      :log_facility                   => 'LOG_FOO',
      :log_dir                        => '/var/log',
      :log_file                       => '/var/log/ceilometer/ceilometer.log',
+     :watch_log_file                 => true,
      :debug                          => true,
     }
   end
@@ -66,6 +67,7 @@ describe 'ceilometer::logging' do
         :syslog_log_facility => '<SERVICE DEFAULT>',
         :log_dir             => '/var/log/ceilometer',
         :log_file            => '<SERVICE DEFAULT>',
+        :watch_log_file      => '<SERVICE DEFAULT>',
         :debug               => '<SERVICE DEFAULT>',
       )
     end
@@ -81,6 +83,7 @@ describe 'ceilometer::logging' do
         :syslog_log_facility => 'LOG_FOO',
         :log_dir             => '/var/log',
         :log_file            => '/var/log/ceilometer/ceilometer.log',
+        :watch_log_file      => true,
         :debug               => true,
       )
     end
