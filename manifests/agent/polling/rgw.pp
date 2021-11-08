7fbd758d72d347c66f3431fc6ed475266b263081
Takashi Kajinami <tkajinam@redhat.com>
2021-11-09 00:28:19 +0900

    Add support for polling Radosgw metrics
    Ceilometer supports polling metrics from the Radosgw service using
    radosgw.* pollsters. This change introduces support for the parameters
    required to use that feature.
    
    Change-Id: I58be60247bba1483f49f830802d422109ff13930

---
+++ manifests/agent/polling/rgw.pp
@@ -0,0 +1,32 @@
+# == Class: ceilometer::agent::polling::rgw
+#
+# Configure rgw parameters
+#
+# === Parameters
+#
+# [*access_key*]
+#  (Optional) Access key for Radosgw Admin.
+#  Defaults to $::os_service_default
+#
+# [*secret_key*]
+#  (Optional) Secret key for Radosgw Admin.
+#  Defaults to $::os_service_default
+#
+# [*implicit_tenants*]
+#  (Optional) Whether RGW uses implicit tenants or not.
+#  Defaults to $::os_service_default
+#
+class ceilometer::agent::polling::rgw (
+  $access_key       = $::os_service_default,
+  $secret_key       = $::os_service_default,
+  $implicit_tenants = $::os_service_default,
+) {
+
+  include ceilometer::deps
+
+  ceilometer_config {
+    'rgw_admin_credentials/access_key': value => $access_key, secret => true;
+    'rgw_admin_credentials/secret_key': value => $secret_key, secret => true;
+    'rgw_client/implicit_tenants':      value => $implicit_tenants;
+  }
+}
