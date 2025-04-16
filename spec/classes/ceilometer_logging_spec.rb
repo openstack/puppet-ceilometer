2367cb9461c4f13b0edcb4263e8c9381be0eaed1
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-04-16 20:45:48 +0900

    Remove watch_log_file
    The option was deprecated during the 2024.2 cycle[1]. Processes may
    fail to start when it is set to True[2] so supporting it is no longer
    useful.
    
    [1] 31603ac6dacd0b47ec9f02d99bfdd0a7b683119c
    [2] b977cb9becbeaaa67bb663bd8db5aa6846e495b9
    
    Change-Id: Ia54b48da9f692d67c4d08a3170417bd636d41e2d

--- spec/classes/ceilometer_logging_spec.rb
+++ spec/classes/ceilometer_logging_spec.rb
@@ -30,7 +30,6 @@ describe 'ceilometer::logging' do
      :log_facility                   => 'LOG_FOO',
      :log_dir                        => '/var/log',
      :log_file                       => '/var/log/ceilometer/ceilometer.log',
-     :watch_log_file                 => true,
      :debug                          => true,
     }
   end
@@ -82,7 +81,6 @@ describe 'ceilometer::logging' do
         :syslog_log_facility => 'LOG_FOO',
         :log_dir             => '/var/log',
         :log_file            => '/var/log/ceilometer/ceilometer.log',
-        :watch_log_file      => true,
         :debug               => true,
       )
     end
